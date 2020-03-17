1bc3523b9e94319d1780ea3f4e3b6dd9f79fab91
songjinxin <506631245@qq.com>
2020-03-17 09:42:45 +0800

    canvas画布

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <canvas id="myCanvas" width="1000" height="800"></canvas>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  mounted() {
+    // 获取元素
+    var canvas = document.getElementById("myCanvas");
+    for (var i = 30; i < 600; i += 30) {
+      for (var k = 30; k < 800; k += 30)
+        if (Math.random() > 0.75) {
+          var box = canvas.getContext("2d");
+          box.fillStyle = "skyblue";
+          box.fillRect(k, i, 20, 20);
+        } else if (Math.random() > 0.5 && Math.random() < 0.75) {
+          var box1 = canvas.getContext("2d");
+          box1.fillStyle = "yellow";
+          box1.fillRect(k, i, 20, 20);
+        } else if (Math.random() > 0.25 && Math.random() < 0.5) {
+          var box2 = canvas.getContext("2d");
+          box2.fillStyle = "pink";
+          box2.fillRect(k, i, 20, 20);
+        } else {
+          var box3 = canvas.getContext("2d");
+          box3.fillStyle = "purple";
+          box3.fillRect(k, i, 20, 20);
+        }
+    }
+  },
+
+};
 </script>
+
+<style scoped>
+#myCanvas {
+  border: 1px solid #ccc;
+}
+</style>
